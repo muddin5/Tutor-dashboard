b76496767d0cfafafb3976439bd91ab4f5fbe4f6
Misbah Uddin <157400048+muddin5@users.noreply.github.com>
2025-07-17 15:44:14 -0400

    1:40:25 NavBar finished, now for homepage

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -7,15 +7,28 @@ const initialState = {
     cart: false,
     userProfile: false,
     notification:false
-}
+};
 
 export const ContextProvider = ( { children } ) => {
+    //Copied directly from Repo while debugging smmth, but uncomment them when we see them in vid
+    const [screenSize, setScreenSize] = useState(undefined);
+    //const [currentColor, setCurrentColor] = useState('#03C9D7');
+    //const [currentMode, setCurrentMode] = useState('Light');
+    //const [themeSettings, setThemeSettings] = useState(false); 
+    
     const [activeMenu, setActiveMenu] = useState (true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true });
+    }
+
     return(
         <StateContext.Provider
         value ={{
             activeMenu,
             setActiveMenu,
+            isClicked, setIsClicked, handleClick,
+            screenSize, setScreenSize,
         }}
         >
             {children}
