f1be090a5e5f40765317cde5caed1de0616d71d3
Taokir Pitam <CYB101EMAIL>
2025-07-20 15:28:22 -0400

    Calender Done with slight limitations 2:43:32

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
+import { Data } from '@syncfusion/ej2-react-grids';
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{dataSource:scheduleData}}
+        selectedDate={new Date(2021,0,10)} //forcing it to be on 2021 jan 10th lol
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
+      </ScheduleComponent>
+    </div>
   )
 }
 
