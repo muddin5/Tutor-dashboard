974850fdc878ba156cf831897b96b870b4238bba
Misbah Uddin <157400048+muddin5@users.noreply.github.com>
2025-07-22 10:01:45 -0400

    Copied Stacked.jsx from another repo, ChatGPT helped format Pie.jsx

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 
-import { Header, LineChart } from '../../components'
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
+const LineChart = () => {
+  const { currentMode } = useStateContext();
 
-const Line = () => {
   return (
-    <div className = "m-4 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="Chart" title="Inflation Rate in Percentage" />
-        <div className="w-full">
-          <LineChart />
-        </div>
-    </div>
-  )
-}
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
 
-export default Line
+export default LineChart;
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationLegend,
+  PieSeries,
+  AccumulationDataLabel,
+  Inject,
+  AccumulationTooltip
+} from '@syncfusion/ej2-react-charts';
+
+import { Header } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { pieChartData } from '../../data/dummy';
 
 const Pie = () => {
-  return (
-    <div>Pie</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Pie
+  return (
+    <div className="m-4 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pie" title="Project Cost Breakdown" />
+
+      <AccumulationChartComponent
+        id="pie-chart"
+        height="420px"
+        tooltip={{ enable: true }}
+        legendSettings={{ visible: true, background: 'white' }}
+        background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      >
+        <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective
+            name="Cost"
+            dataSource={pieChartData}
+            xName="x"
+            yName="y"
+            radius="70%"
+            explode
+            explodeOffset="10%"
+            explodeIndex={0}
+            dataLabel={{
+              visible: true,
+              name: 'text',
+              position: 'Inside',
+              font: {
+                fontWeight: '600',
+                color: '#fff',
+              },
+            }}
+          />
+        </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>
+    </div>
+  );
+};
+
+export default Pie;
